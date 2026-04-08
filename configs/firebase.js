28f4a16632991772f3a503160882710f0da4fd93
IanAlfaroSantos <ialfasa2020@gmail.com>
2026-04-08 11:07:20 -0600

    Version 1

--- configs/firebase.js
+++ configs/firebase.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 export const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY || 'AIzaSyAJqZlm8vk27pkLnBfrojSyGITdCZBQIaw',
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN || 'mi-taller-pro-gtq.firebaseapp.com',
-  projectId: process.env.FIREBASE_PROJECT_ID || 'mi-taller-pro-gtq',
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET || 'mi-taller-pro-gtq.firebasestorage.app',
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || '1096958123404',
-  appId: process.env.FIREBASE_APP_ID || '1:1096958123404:web:cd69bbc0966ebfefce4aa9'
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 const FIRESTORE_BASE_URL = `https://firestore.googleapis.com/v1/projects/${firebaseConfig.projectId}/databases/(default)/documents`;
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { createServer } from './configs/server.js';
 
 const app = createServer();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`SenGarage backend running on ${port}`);
 });
